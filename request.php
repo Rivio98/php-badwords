2f91ce079c22af7773a31b8354b6fe5b1be33931
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-04 15:54:35 +0200

    [UPD] Add get

---
+++ request.php
@@ -0,0 +1,21 @@
+<?php
+$text = $_GET["text"];
+$word = $_GET["word"];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-request</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
